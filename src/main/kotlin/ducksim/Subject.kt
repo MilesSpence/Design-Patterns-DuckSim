a0ec7cf60c4508de86b3fe7cf19c27960b5684ea
Miles Spence <mspence@mail.umw.edu>
2021-12-30 01:24:11 -0500

    Add files via upload

---
+++ src/main/kotlin/ducksim/Subject.kt
@@ -0,0 +1,18 @@
+package ducksim
+
+abstract class Subject {
+    private var observers = ArrayList<Observer>()
+    fun registerObserver(o: Observer) {
+        observers.add(o)
+    }
+
+    fun removeObserver(o: Observer) {
+        observers.remove(o)
+    }
+
+    fun notifyObserver() {
+        for (observer in observers) {
+            observer.update()
+        }
+    }
+}
